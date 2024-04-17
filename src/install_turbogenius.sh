5df34f4ecec62130c2ca60df4f22207b5deaa245
Ot(t)o Kohulák <pravod@gmail.com>
2024-04-17 22:41:31 +0200

    Add recipe for TurboRVB

---
+++ src/install_turbogenius.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+cd /opt
+source environment.sh
+
+apt install -y python3 python3-pip git
+ln -sf /usr/bin/python3 /usr/bin/python
+
+pip install trexio
+
+git clone --depth=1 https://github.com/kousuke-nakano/turbogenius.git
+cd turbogenius
+pip install .
+cd ..
+
+rm -rf turbogenius
+
