92c3fa0455826faaa8623337e1fb16c63250d71a
Ot(t)o Kohulák <pravod@gmail.com>
2024-04-17 22:52:47 +0200

    Remove unecessary line

--- src/install_turbogenius.sh
+++ src/install_turbogenius.sh
@@ -6,8 +6,6 @@ source environment.sh
 apt install -y python3 python3-pip git
 ln -sf /usr/bin/python3 /usr/bin/python
 
-pip install trexio
-
 git clone --depth=1 https://github.com/kousuke-nakano/turbogenius.git
 cd turbogenius
 pip install .
