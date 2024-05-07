fa9a1a6df2edea354b46e17d1ca109167d05b6da
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2024-05-07 12:32:36 +0200

    Fix GAMMCOR on arm

--- src/install_gammcor.sh
+++ src/install_gammcor.sh
@@ -38,14 +38,14 @@ cd xcfun
 sed -i "s/g++/g++-12 -fPIC -march=armv8-a/g" Makefile.gcc
 sed -i "s/gcc/gcc-12 -fPIC -march=armv8-a/g" Makefile.gcc
 sed -i "s/gfortran/gfortran-12 -fPIC -march=armv8-a/g" Makefile.gcc
-rm -f src/functionallist_of_functionals.o
+rm -f src/*/*.o
 make -f Makefile.gcc
 cd ..
 
 
 cat << EOF > Makefile
 FCC = gfortran-12 -march=armv8-a 
-FFLAGS = -O3 -g -I xcfun/fortran -fopenmp -std=legacy -fallow-argument-mismatch
+FFLAGS = -O3 -g -I xcfun/fortran -fopenmp -fallow-argument-mismatch
 LIBS = -L./xcfun/lib/ -lxcfun -lopenblas
 
 include Makefile.common
